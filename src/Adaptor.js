283de6e927248c677e0d4c49c9bc097cf00f86d4
Taylor Downs <downs.taylor@gmail.com>
2020-04-24 10:19:59 -0600

    v1.0.0 supports node 12

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,8 +1,6 @@
 import { execute as commonExecute, expandReferences } from 'language-common';
-import request from 'request'
+import request from 'request';
 import { resolve as resolveUrl } from 'url';
-import Adaptor from 'language-http';
-const { get, post } = Adaptor;
 /** @module Adaptor */
 
 /**
@@ -20,15 +18,16 @@ const { get, post } = Adaptor;
 export function execute(...operations) {
   const initialState = {
     references: [],
-    data: null
-  }
-
-  return state => {
-    state.configuration.authType = 'digest';
-    state.configuration.baseUrl = "https://".concat(state.configuration.instanceName, ".surveycto.com/api/v1")
-    return commonExecute(...operations)({ ...initialState, ...state })
+    data: null,
   };
 
+  return (state) => {
+    state.configuration.baseUrl = 'https://'.concat(
+      state.configuration.instanceName,
+      '.surveycto.com/api/v1'
+    );
+    return commonExecute(...operations)({ ...initialState, ...state });
+  };
 }
 
 /**
@@ -42,38 +41,72 @@ export function execute(...operations) {
  * @returns {Operation}
  */
 export function fetchSubmissions(formId, afterDate, postUrl) {
-  return get(`forms/data/wide/json/${ formId }`, {
-    query: function(state) {
-      console.log("baseUrl: ".concat(state.configuration.baseUrl))
-      return { date: state.lastSubmissionDate || afterDate }
-    },
-    callback: function(state) {
-      // Pick submissions out in order to avoid `post` overwriting `response`.
-      var submissions = state.response.body;
-      // return submissions
-      return submissions.reduce(function(acc, item) {
-        // tag submissions as part of the identified form
-        item.formId = formId;
-        return acc.then(
-          post( postUrl, { body: item })
-        )
-      }, Promise.resolve(state))
-        .then(function(state) {
-          if (submissions.length) {
-            state.lastSubmissionDate = submissions[submissions.length-1].SubmissionDate
+  return (state) => {
+    const { baseUrl, username, password, instanceName } = state.configuration;
+
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          url: `${baseUrl}/forms/data/wide/json/${formId}`,
+          method: 'GET',
+          auth: {
+            username,
+            password,
+            sendImmediately: false,
+          },
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          query: {
+            date: state.lastSubmissionDate || afterDate,
+          },
+        },
+        function (error, response, body) {
+          if (error) {
+            return console.error('Get failed:', error);
           }
-          return state;
-        })
-        .then(function(state) {
-          delete state.response
-          console.log("fetchSubmissions succeeded.")
-          return state;
-        })
-    }
-  })
+          console.log('Get succeeded');
+          // Pick submissions out in order to avoid `post` overwriting `response`.
+          var submissions = JSON.parse(response.body);
+
+          // return submissions
+          return submissions
+            .reduce(function (acc, item) {
+              // tag submissions as part of the identified form
+              item.formId = formId;
+              return acc.then(
+                request({
+                  url: postUrl,
+                  method: 'POST',
+                  json: item,
+                })
+              );
+            }, Promise.resolve(state))
+            .then(function (state) {
+              if (submissions.length) {
+                state.lastSubmissionDate =
+                  submissions[submissions.length - 1].SubmissionDate;
+              }
+              return state;
+            })
+            .then(function (state) {
+              delete state.response;
+              console.log('fetchSubmissions succeeded.');
+              return state;
+            });
+        }
+      );
+    });
+  };
 }
 
 export {
-  field, fields, sourceValue, fields, alterState,
-  merge, dataPath, dataValue, lastReferenceValue
+  field,
+  fields,
+  sourceValue,
+  alterState,
+  merge,
+  dataPath,
+  dataValue,
+  lastReferenceValue,
 } from 'language-common';
